6340cf6e1e343bed4d21ad6d0b10291df6526142
nareshsrkr1 <138985162+nareshsrkr1@users.noreply.github.com>
2023-08-17 13:16:54 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,69 +1,25 @@
-from pyspark.sql import SparkSession
-
-class SparkManager:
-    def __init__(self, app_name, db_config):
-        self.app_name = app_name
-        self.db_config = db_config
-        self.spark = self._create_spark_session()
-
-    def _create_spark_session(self):
-        return SparkSession.builder \
-            .appName(self.app_name) \
-            .config("spark.some.config.option", "config_value") \
-            .getOrCreate()
-
-    def read_db_config(self):
-        return self.db_config
-
-
-from SparkManager import SparkManager
-
-class SparkDatabaseUtility:
-    def __init__(self, spark_manager):
-        self.spark_manager = spark_manager
-
-    def execute_sql(self, sql_query):
-        result = self.spark_manager.spark.sql(sql_query)
-        return result
-
-    def write_to_database(self, data_frame, table_name):
-        data_frame.write \
-            .format("jdbc") \
-            .option("url", self.spark_manager.db_config["url"]) \
-            .option("dbtable", table_name) \
-            .option("user", self.spark_manager.db_config["user"]) \
-            .option("password", self.spark_manager.db_config["password"]) \
-            .save()
-
-
+import yaml
 from SparkManager import SparkManager
 from SparkDatabaseUtility import SparkDatabaseUtility
 
-# Define your database configuration
+# Read all configurations from YAML file
+with open("db_config.yml", "r") as yml_file:
+    all_config = yaml.safe_load(yml_file)
+
+# Choose the environment (uat or prd)
+environment = "uat"  # Change this to "prd" for production
+
+# Get the environment-specific configuration
+environment_config = all_config.get(environment, {})
+
+# Merge common configuration with environment-specific configuration
+common_config = all_config.get("common", {})
 db_config = {
-    "url": "jdbc:postgresql://localhost:5432/mydb",
-    "user": "username",
-    "password": "password"
+    "driver_batch_size": common_config.get("driver_batch_size", 100),
+    **environment_config
 }
 
 # Create SparkManager instance
 spark_manager = SparkManager("MySparkApp", db_config)
 
-# Read database configuration
-db_config_read = spark_manager.read_db_config()
-print("Database Configuration:", db_config_read)
-
-# Create SparkDatabaseUtility instance
-db_utility = SparkDatabaseUtility(spark_manager)
-
-# Execute SQL query
-sql_query = "SELECT * FROM mytable"
-result_df = db_utility.execute_sql(sql_query)
-result_df.show()
-
-# Write data to database
-data_to_write = ...
-table_name = "mytable"
-db_utility.write_to_database(data_to_write, table_name)
-
-
+# ... Rest of the code remains the same ...
