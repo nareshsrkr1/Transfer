5a4f1e21166fe153756f67667fb99abedef6b44d
nareshsrkr1 <138985162+nareshsrkr1@users.noreply.github.com>
2023-09-06 17:12:56 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -9,11 +9,17 @@ extract_datestamp() {
 }
 
 # Get a list of unique datestamps from the filenames in the directory
-datestamps=$(find . -type f -name "*sr14*.dat" | while read -r file; do extract_datestamp "$file"; done | sort -u)
+datestamps=$(find . -type f -name "*sr14*.dat" -o -name "*sr14*.ctl" | while read -r file; do extract_datestamp "$file"; done | sort -u)
 
 # Loop through each datestamp
 for datestamp in $datestamps; do
     # Create a tarball for files with the current datestamp
     tar_filename="${datestamp}.tar.gz"
-    find . -type f -name "*sr14*${datestamp}.dat" -exec tar -cvzf "$tar_filename" {} +
+    find . -type f \( -name "*sr14*${datestamp}.dat" -o -name "*sr14*${datestamp}.ctl" \) -exec tar -rvf "$tar_filename" {} \;
+done
+
+# Compress the tarballs
+for datestamp in $datestamps; do
+    tar_filename="${datestamp}.tar"
+    gzip "$tar_filename"
 done
