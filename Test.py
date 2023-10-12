0444772a17fb98edaea9aa79c599100526d4e890
nareshsrkr1 <138985162+nareshsrkr1@users.noreply.github.com>
2023-10-13 00:16:59 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,25 +1,97 @@
-#!/bin/bash
+import os
+import pyodbc
+import threading
+import subprocess
+import time
 
-# Function to extract the date timestamp from a file name
-extract_datestamp() {
-    filename="$1"
-    # Extract the datestamp (YYYYMMDD) from the filename
-    datestamp=$(echo "$filename" | grep -oP '\d{8}')
-    echo "$datestamp"
-}
+# Function to send an event to an Autosys job
+def send_event(job_name, event_type):
+    send_event_command = f"sendevent -E {event_type} -J {job_name}"
+    subprocess.run(send_event_command, shell=True)
 
-# Get a list of unique datestamps from the filenames in the directory
-datestamps=$(find . -type f -name "*sr14*.dat" -o -name "*sr14*.ctl" | while read -r file; do extract_datestamp "$file"; done | sort -u)
+# Function to check the status of an Autosys job
+def check_job_status(job_name):
+    autostatus_command = f"autostatus -J {job_name}"
+    result = subprocess.run(autostatus_command, shell=True, capture_output=True, text=True)
+    status_output = result.stdout
+    return status_output
 
-# Loop through each datestamp
-for datestamp in $datestamps; do
-    # Create a tarball for files with the current datestamp
-    tar_filename="${datestamp}.tar.gz"
-    find . -type f \( -name "*sr14*${datestamp}.dat" -o -name "*sr14*${datestamp}.ctl" \) -exec tar -rvf "$tar_filename" {} \;
-done
+# Function to set the profile based on the user (Unix username)
+def set_profile(username):
+    if username == "prod":
+        profile = "/opt/autosys.pb3"
+    else:
+        profile = "/opt/autosys.ub3"
+    return profile
 
-# Compress the tarballs
-for datestamp in $datestamps; do
-    tar_filename="${datestamp}.tar"
-    gzip "$tar_filename"
-done
+# Function to invoke the shell script to set the profile
+def invoke_profile_script(profile):
+    subprocess.run(f"source {profile}", shell=True)
+
+# Function to execute a job (placeholder for your job execution logic)
+def execute_job(job_id, job_type, job_name):
+    print(f"Executing Job {job_id} ({job_type})")
+    # Here, you can replace this with actual job execution commands
+    # e.g., subprocess.run(['command_to_execute', job_name])
+
+# Database connection settings
+server = 'YourServerName'
+database = 'YourDatabaseName'
+username = 'YourUsername'
+password = 'YourPassword'
+connection_string = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}'
+
+# Get the current Unix username
+user = os.getlogin()
+
+# Set the profile based on the user (Unix username)
+profile = set_profile(user)
+
+# Invoke the shell script to set the profile
+invoke_profile_script(profile)
+
+# Connect to the database
+conn = pyodbc.connect(connection_string)
+cursor = conn.cursor()
+
+# Query the AutosysJobs table to retrieve job information, including the event to send and dependencies
+cursor.execute("SELECT JobID, JobType, ExecutionOrder, DependsOn, EventToSend FROM AutosysJobs ORDER BY ExecutionOrder")
+job_info = cursor.fetchall()
+
+# Close the cursor and connection
+cursor.close()
+conn.close()
+
+# Create and start threads for jobs
+threads = []
+
+# Dictionary to store job statuses
+job_statuses = {}
+
+for job_id, job_type, execution_order, depends_on, event_type in job_info:
+    job_statuses[job_id] = "PENDING"
+
+def check_dependencies_completed(job_id, depends_on):
+    while job_statuses[depends_on] != "SUCCESS":
+        time.sleep(5)
+
+    job_statuses[job_id] = "PENDING"
+
+# Start threads to check dependencies and execute jobs
+for job_id, job_type, execution_order, depends_on, event_type in job_info:
+    if not depends_on:
+        # This job has no dependency, start it
+        send_event(job_id, event_type)
+        job_thread = threading.Thread(target=execute_job, args=(job_id, job_type, job_id))
+        job_statuses[job_id] = "PENDING"
+        job_thread.start()
+    else:
+        # This job has a dependency, check if the dependency is successful
+        dependency_check_thread = threading.Thread(target=check_dependencies_completed, args=(job_id, depends_on))
+        dependency_check_thread.start()
+
+# Wait for all threads to complete
+for thread in threads:
+    thread.join()
+
+# The rest of your script continues here...
