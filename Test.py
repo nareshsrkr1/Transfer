245228e0312fc3eb72b8c65c38df85bfa6fdfa63
nareshsrkr1 <138985162+nareshsrkr1@users.noreply.github.com>
2023-12-05 23:28:14 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -2,12 +2,10 @@
 
 <div class="container-fluid d-flex align-items-center justify-content-center">
   <div class="form-group">
-    <label for="businessDate">Business Date:</label>
-    <div class="input-group">
+    <div class="d-flex align-items-center">
+      <label for="businessDate" class="mr-2">Business Date:</label>
       <input type="date" id="businessDate" [(ngModel)]="selectedDate" class="form-control" required>
-      <div class="input-group-append">
-        <button (click)="onSubmit()" class="btn btn-primary">Submit</button>
-      </div>
     </div>
+    <button (click)="onSubmit()" class="btn btn-primary mt-3">Submit</button>
   </div>
 </div>
