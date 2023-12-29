edcaab72760d7c2230729196451fb4f08ca789d7
nareshsrkr1 <138985162+nareshsrkr1@users.noreply.github.com>
2023-12-30 00:07:06 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,3 +1,4 @@
+import os
 import psutil
 import json
 import yaml
@@ -7,96 +8,62 @@ def read_config(filename='config.yml'):
         config = yaml.safe_load(file)
     return config
 
-def get_cpu_usage():
-    return psutil.cpu_percent(interval=1, percpu=True)
+def get_folder_size(folder_path):
+    total_size = 0
+    for dirpath, dirnames, filenames in os.walk(folder_path):
+        for filename in filenames:
+            filepath = os.path.join(dirpath, filename)
+            total_size += os.path.getsize(filepath)
+    return total_size / (2**30)  # Convert to GB
 
-def get_memory_info():
-    memory = psutil.virtual_memory()
-    available = round(memory.available/1024.0/1024.0, 1)
-    total = round(memory.total/1024.0/1024.0, 1)
-    return {
-        'available': f"{available} MB",
-        'total': f"{total} MB",
-        'percent': f"{memory.percent}%"
-    }
+def get_top_files(folder_path, num_files=5):
+    files_info = []
+    for dirpath, dirnames, filenames in os.walk(folder_path):
+        file_sizes = [(os.path.join(dirpath, filename), os.path.getsize(os.path.join(dirpath, filename))) for filename in filenames]
+        file_sizes.sort(key=lambda x: x[1], reverse=True)
+        top_files = file_sizes[:num_files]
+        files_info.extend(top_files)
+    return files_info
 
-def get_disk_usage(mount_path):
-    disk = psutil.disk_usage(mount_path)
-    free = round(disk.free/1024.0/1024.0/1024.0, 1)
-    used = round(disk.used/1024.0/1024.0/1024.0, 1)
-    total = round(disk.total/1024.0/1024.0/1024.0, 1)
-    return {
-        'free': f"{free} GB",
-        'used': f"{used} GB",
-        'total': f"{total} GB",
-        'percent': f"{disk.percent}%"
-    }
+def get_app_server_space(config):
+    app_space_info = {}
 
-def get_process_info(process):
-    try:
-        process_exe = psutil.Process(process['pid']).exe()
-    except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-        process_exe = "N/A"
-    return {
-        'pid': process['pid'],
-        'name': process['name'],
-        'username': process['username'],
-        'cpu_percent': f"{process['cpu_percent']}%",
-        'exe': process_exe
-    }
+    for app_config in config['app_servers']:
+        app_name = app_config['name']
+        app_paths = app_config['paths']
 
-def get_top_cpu_user_id():
-    processes = psutil.process_iter(['pid', 'name', 'username', 'cpu_percent'])
-    user_processes = [p.info for p in processes if p.info['username'] != '']
-    if not user_processes:
-        return None
-    sorted_user_processes = sorted(user_processes, key=lambda x: x['cpu_percent'], reverse=True)
-    return sorted_user_processes[0]['username']
+        app_space_info[app_name] = {}
 
-def get_top_cpu_processes(user_id, num_processes=10):
-    processes = psutil.process_iter(['pid', 'name', 'username', 'cpu_percent'])
-    user_processes = [p.info for p in processes if p.info['username'] == user_id]
-    sorted_user_processes = sorted(user_processes, key=lambda x: x['cpu_percent'], reverse=True)[:num_processes]
-    return [get_process_info(process) for process in sorted_user_processes]
+        for path_config in app_paths:
+            path_name = path_config['name']
+            path = path_config['path']
+
+            # Get folder size
+            folder_size = get_folder_size(path)
+
+            # Get top 5 most occupied files
+            top_files = get_top_files(path)
+
+            # Add information to the app_space_info dictionary
+            app_space_info[app_name][path_name] = {
+                'folder_size': folder_size,
+                'top_files': [{'file': file[0], 'size': file[1]} for file in top_files]
+            }
+
+    return app_space_info
 
 def get_server_stats(config):
     stats = {}
 
-    # Get server-level CPU and memory information
-    cpu_stats = get_cpu_usage()
-    
-    # Determine the top CPU user ID
-    top_cpu_user_id = config['server'].get('top_cpu_user_id', '')
-    if not top_cpu_user_id:
-        top_cpu_user_id = get_top_cpu_user_id()
+    # ... (existing code for CPU, memory, mounts)
 
-    # Limit the number of top CPU processes to 10
-    top_cpu_processes = get_top_cpu_processes(top_cpu_user_id, num_processes=10)
-
-    stats['Server Level Stats'] = {
-        'CPU Info': f"{cpu_stats}%",  # Display overall CPU usage
-        'Memory Info': get_memory_info(),
-        'Mounts': {},
-        'Top CPU User ID': top_cpu_user_id,
-        'Top CPU Processes': top_cpu_processes
-    }
-
-    # Iterate through mounts in the configuration
-    for mount_path in config['server']['mounts']:
-        # Get disk usage for the mount
-        mount_disk_stats = get_disk_usage(mount_path)
-
-        # Add mount disk usage to the stats
-        stats['Server Level Stats']['Mounts'][mount_path] = {
-            'path': mount_path,
-            'disk_usage': mount_disk_stats
-        }
+    # Add app server space information
+    app_server_space = get_app_server_space(config)
+    stats['App Server Space'] = app_server_space
 
     return stats
 
-def save_to_json(data, filename='server_stats.json'):
-    with open(filename, 'w') as file:
-        json.dump(data, file, indent=4)
+# ... (remaining code remains the same)
 
 if __name__ == "__main__":
     config = read_config()
