3b28e388ed2080354e204e633588de661ec4c7a5
nareshsrkr1 <138985162+nareshsrkr1@users.noreply.github.com>
2023-12-29 21:18:21 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,7 +1,6 @@
 import psutil
 import json
 import yaml
-import time
 
 def read_config(filename='config.yml'):
     with open(filename, 'r') as file:
@@ -14,9 +13,9 @@ def get_cpu_usage():
 def get_disk_usage(folder_path):
     disk_usage = psutil.disk_usage(folder_path)
     return {
-        'total': disk_usage.total,
-        'used': disk_usage.used,
-        'free': disk_usage.free
+        'total': disk_usage.total / (2**30),  # Convert to GB
+        'used': disk_usage.used / (2**30),
+        'free': disk_usage.free / (2**30)
     }
 
 def get_server_stats(config):
@@ -28,13 +27,16 @@ def get_server_stats(config):
     # Include overall CPU usage in the stats
     stats['cpu_usage'] = cpu_stats
 
+    # App Server Space Summary section
+    app_space_summary = {}
+
     for app_config in config['apps']:
         app_name = app_config['name']
         folder_path = app_config['folder_path']
-        
+
         # Get CPU usage for the specific app
         app_cpu_stats = cpu_stats if app_name == 'overall' else get_cpu_usage()
-        
+
         # Get disk usage for the app
         app_disk_stats = get_disk_usage(folder_path)
 
@@ -44,22 +46,24 @@ def get_server_stats(config):
             'disk_usage': app_disk_stats
         }
 
+        # Add app disk usage to the summary
+        app_space_summary[app_name] = {
+            'total': app_disk_stats['total'],
+            'used': app_disk_stats['used'],
+            'free': app_disk_stats['free']
+        }
+
+    # Add the App Server Space Summary section
+    stats['App Server Space Summary'] = app_space_summary
+
     return stats
 
 def save_to_json(data, filename='server_stats.json'):
     with open(filename, 'w') as file:
         json.dump(data, file, indent=4)
 
-def main():
-    config = read_config()
-
-    while True:
-        server_stats = get_server_stats(config)
-        save_to_json(server_stats)
-
-        # Adjust the sleep time based on your desired interval
-        time.sleep(60)  # Sleep for 60 seconds
-
 if __name__ == "__main__":
-    main()
+    config = read_config()
+    server_stats = get_server_stats(config)
+    save_to_json(server_stats)
     
