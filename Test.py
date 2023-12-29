0d52e725e5ef6bcf681273a00f0a86de60cb1c22
nareshsrkr1 <138985162+nareshsrkr1@users.noreply.github.com>
2023-12-30 00:40:12 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,11 +1,32 @@
 import os
 
+def get_folder_size(folder_path):
+    total_size = 0
+    for dirpath, dirnames, filenames in os.walk(folder_path, followlinks=True):
+        for filename in filenames:
+            filepath = os.path.join(dirpath, filename)
+            if os.path.exists(filepath):  # Check if the file or symlink target exists
+                total_size += os.path.getsize(filepath)
+    return total_size / (2**30)  # Convert to GB
+
+def get_top_files(folder_path, num_files=5):
+    files_info = []
+    for dirpath, dirnames, filenames in os.walk(folder_path, followlinks=True):
+        for filename in filenames:
+            filepath = os.path.join(dirpath, filename)
+            if os.path.exists(filepath):  # Check if the file or symlink target exists
+                files_info.append((filepath, os.path.getsize(filepath)))
+    files_info.sort(key=lambda x: x[1], reverse=True)
+    return files_info[:num_files]
+
 def get_top_folder_sizes(base_path, num_folders=10):
     folder_sizes = []
 
-    for dirpath, dirnames, filenames in os.walk(base_path):
-        folder_size = sum(os.path.getsize(os.path.join(dirpath, filename)) for filename in filenames)
-        folder_sizes.append((dirpath, folder_size))
+    for dirpath, dirnames, filenames in os.walk(base_path, followlinks=True):
+        for filename in filenames:
+            filepath = os.path.join(dirpath, filename)
+            if os.path.exists(filepath):  # Check if the file or symlink target exists
+                folder_sizes.append((dirpath, os.path.getsize(filepath)))
 
     # Sort folders based on size in descending order
     sorted_folders = sorted(folder_sizes, key=lambda x: x[1], reverse=True)
@@ -17,36 +38,4 @@ def get_top_folder_sizes(base_path, num_folders=10):
     top_folders_in_gb = [(folder[0], folder[1] / (2**30)) for folder in top_folders]
 
     return top_folders_in_gb
-
-
-def get_app_server_space(config):
-    app_space_info = {}
-
-    for app_config in config['app_servers']:
-        app_name = app_config['name']
-        app_paths = app_config['paths']
-
-        app_space_info[app_name] = {}
-
-        for path_config in app_paths:
-            path_name = path_config['name']
-            path = path_config['path']
-
-            # Get folder size
-            folder_size = get_folder_size(path)
-
-            # Get top 5 most occupied files
-            top_files = get_top_files(path)
-
-            # Get top 10 occupied folder sizes
-            top_folders = get_top_folder_sizes(path, num_folders=10)
-
-            # Add information to the app_space_info dictionary
-            app_space_info[app_name][path_name] = {
-                'folder_size': folder_size,
-                'top_files': [{'file': file[0], 'size': file[1]} for file in top_files],
-                'top_folders': [{'folder': folder[0], 'size': folder[1]} for folder in top_folders]
-            }
-
-    return app_space_info
     
