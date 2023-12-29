3ca463a3e31bc9160243da278d265fb3ce43db22
nareshsrkr1 <138985162+nareshsrkr1@users.noreply.github.com>
2023-12-29 20:21:28 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,57 +1,65 @@
-import os
 import psutil
-import pandas as pd
-from configparser import ConfigParser
-from sqlalchemy import create_engine
+import json
+import yaml
+import time
 
-def read_config():
-    config = ConfigParser()
-    config.read('config.ini')
-    return config['Database']
+def read_config(filename='config.yml'):
+    with open(filename, 'r') as file:
+        config = yaml.safe_load(file)
+    return config
 
-def get_top_n_largest(path, n=5):
-    # Get the top N largest folders/files in the specified path
-    top_n = []
-    for root, dirs, files in os.walk(path, topdown=False):
-        for name in files:
-            full_path = os.path.join(root, name)
-            size = os.path.getsize(full_path)
-            top_n.append((full_path, size))
+def get_cpu_usage():
+    return psutil.cpu_percent(interval=1, percpu=True)
 
-        for name in dirs:
-            full_path = os.path.join(root, name)
-            size = sum(os.path.getsize(os.path.join(dirpath, filename)) for dirpath, dirnames, filenames in os.walk(full_path) for filename in filenames)
-            top_n.append((full_path, size))
+def get_disk_usage(folder_path):
+    disk_usage = psutil.disk_usage(folder_path)
+    return {
+        'total': disk_usage.total,
+        'used': disk_usage.used,
+        'free': disk_usage.free
+    }
 
-    top_n.sort(key=lambda x: x[1], reverse=True)
-    return top_n[:n]
+def get_server_stats(config):
+    stats = {}
 
-def collect_and_insert():
-    db_config = read_config()
+    # Get overall CPU usage
+    cpu_stats = get_cpu_usage()
 
-    cpu_usage = get_cpu_usage()
-    cpu_df = pd.DataFrame(cpu_usage, columns=[f'Core_{i}' for i in range(len(cpu_usage))])
+    # Include overall CPU usage in the stats
+    stats['cpu_usage'] = cpu_stats
 
-    memory_usage = get_memory_usage()
-    memory_df = pd.DataFrame([memory_usage])
+    for app_config in config['apps']:
+        app_name = app_config['name']
+        folder_path = app_config['folder_path']
+        
+        # Get CPU usage for the specific app
+        app_cpu_stats = cpu_stats if app_name == 'overall' else get_cpu_usage()
+        
+        # Get disk usage for the app
+        app_disk_stats = get_disk_usage(folder_path)
 
-    disk_usage = get_disk_usage()
-    disk_df = pd.DataFrame(disk_usage).T
+        # Include CPU and disk usage for each app in the stats
+        stats[app_name] = {
+            'cpu_usage': app_cpu_stats,
+            'disk_usage': app_disk_stats
+        }
 
-    network_stats = get_network_stats()
-    network_df = pd.DataFrame(network_stats).T
+    return stats
 
-    result_df = pd.concat([cpu_df, memory_df, disk_df, network_df], axis=1)
+def save_to_json(data, filename='server_stats.json'):
+    with open(filename, 'w') as file:
+        json.dump(data, file, indent=4)
 
-    # Connect to the database and insert data
-    engine = create_engine(f"{db_config['engine']}://{db_config['user']}:{db_config['password']}@{db_config['host']}:{db_config['port']}/{db_config['database']}")
-    result_df.to_sql('server_stats', con=engine, if_exists='replace', index=False)
+def main():
+    config = read_config()
 
-    # Get and print the top N largest folders/files on a specific path (replace 'YOUR_PATH' with the desired path)
-    top_n = get_top_n_largest('YOUR_PATH', n=5)
-    print(f"\nTop {len(top_n)} Largest Folders/Files:")
-    for item in top_n:
-        print(f"{item[0]}: {item[1]} bytes")
+    while True:
+        server_stats = get_server_stats(config)
+        save_to_json(server_stats)
+
+        # Adjust the sleep time based on your desired interval
+        time.sleep(60)  # Sleep for 60 seconds
 
 if __name__ == "__main__":
-    collect_and_insert()
+    main()
+    
