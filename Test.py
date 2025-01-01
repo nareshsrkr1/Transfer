609a1832d42c000938562a16567beb9ed92a4c03
nareshsrkr1 <138985162+nareshsrkr1@users.noreply.github.com>
2025-01-02 00:14:20 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,11 +1,37 @@
 DECLARE @JsonString NVARCHAR(MAX);
 
 -- Construct JSON using alias names for hash ID calculation
-SELECT @JsonString = STRING_AGG('"' + AliasName + '":"' + ColumnName + '"', ',')
+SELECT @JsonString = STRING_AGG(
+    '"' + 
+    CASE 
+        WHEN CHARINDEX(' AS ', ColumnExpression) > 0 
+        THEN RIGHT(ColumnExpression, LEN(ColumnExpression) - CHARINDEX(' AS ', ColumnExpression) - 3)
+        ELSE ColumnExpression
+    END 
+    + '":"'
+    +
+    CASE 
+        WHEN CHARINDEX(' AS ', ColumnExpression) > 0 
+        THEN LEFT(ColumnExpression, CHARINDEX(' AS ', ColumnExpression) - 1)
+        ELSE ColumnExpression
+    END 
+    + '"', ',')
 FROM (
     SELECT 
-        REPLACE(AliasName, 'Exit_Period', 'business_input') AS AliasName,
-        ColumnName
+        REPLACE(
+            CASE 
+                WHEN CHARINDEX(' AS ', ColumnExpression) > 0 
+                THEN RIGHT(ColumnExpression, LEN(ColumnExpression) - CHARINDEX(' AS ', ColumnExpression) - 3)
+                ELSE ColumnExpression
+            END, 
+            'Exit_Period', 
+            'business_input'
+        ) AS AliasName,
+        CASE 
+            WHEN CHARINDEX(' AS ', ColumnExpression) > 0 
+            THEN LEFT(ColumnExpression, CHARINDEX(' AS ', ColumnExpression) - 1)
+            ELSE ColumnExpression
+        END AS ColumnName
     FROM @Finallist
 ) AS JsonColumns;
 
