59f20743973a6f9ae636478a8e56d790538b80d7
nareshsrkr1 <138985162+nareshsrkr1@users.noreply.github.com>
2025-01-02 01:09:01 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -5,12 +5,12 @@ DECLARE @DynamicQuery NVARCHAR(MAX);
 -- Step 2: Construct dynamic SQL for building JSON string
 SET @DynamicQuery = '
     SELECT @HashIdJson = ''{'' + STRING_AGG(
-        ''"'' + fc.AliasName + ''":"'' +
-        ISNULL(CAST(u.'' + QUOTENAME(fc.ColumnName) + '' AS NVARCHAR(MAX)), '''') + ''''',' +
-        ''''') + ''}'' ' +
-    'FROM FinalColumns fc ' + 
-    'JOIN UNFD u ON fc.ColumnName = u.ColumnName ' + 
-    'ORDER BY fc.SortOrder'; -- Ensures the columns are ordered based on the SortOrder
+        ''"'' + fc.AliasName + ''" : '' +
+        ISNULL(CAST(u.''' + fc.ColumnName + ''' AS NVARCHAR(MAX)), '''') + ''',' +
+        ''''') + ''}'' 
+    FROM FinalColumns fc 
+    JOIN UNFD u ON fc.ColumnName = u.ColumnName 
+    ORDER BY fc.SortOrder'; -- Ensures the columns are ordered based on the SortOrder
 
 -- Debug: Print dynamic query for review
 PRINT @DynamicQuery;
