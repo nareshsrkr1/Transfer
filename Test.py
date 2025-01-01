7fc3bf498eef2982d084de512126f92714725d12
nareshsrkr1 <138985162+nareshsrkr1@users.noreply.github.com>
2025-01-02 00:09:08 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,19 +1,13 @@
-INSERT INTO @FinalColumns (ColumnExpression, SortOrder)
-SELECT 
-    -- Add aliases if mapped, otherwise use the column name directly
-    CASE 
-        WHEN m.OverrideColumn IS NOT NULL THEN u.ColumnName + ' AS [' + m.OverrideColumn + ']'
-        ELSE u.ColumnName
-    END AS ColumnExpression,
-    -- Case-insensitive search for the column in @OrderedColumnList
-    CASE 
-        WHEN CHARINDEX(',' + LOWER(ISNULL(m.OverrideColumn, u.ColumnName)) + ',', ',' + LOWER(@OrderedColumnList) + ',') > 0 THEN 
-            CHARINDEX(',' + LOWER(ISNULL(m.OverrideColumn, u.ColumnName)) + ',', ',' + LOWER(@OrderedColumnList) + ',')
-        ELSE 9999 -- Assign a large number for unmatched columns
-    END AS SortOrder
-FROM @ActiveColumns u
-LEFT JOIN @MappedColumns m
-    ON LOWER(u.ColumnName) = LOWER(m.UnfdColumn); -- Normalize for case-insensitive join
+DECLARE @JsonString NVARCHAR(MAX);
 
--- Debug: Output results to verify
-SELECT * FROM @FinalColumns;
+-- Construct JSON using alias names for hash ID calculation
+SELECT @JsonString = STRING_AGG('"' + AliasName + '":"' + ColumnName + '"', ',')
+FROM (
+    SELECT 
+        REPLACE(AliasName, 'Exit_Period', 'business_input') AS AliasName,
+        ColumnName
+    FROM @Finallist
+) AS JsonColumns;
+
+SET @JsonString = '{' + @JsonString + '}';
+PRINT @JsonString; -- Debug output
