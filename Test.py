dd4c3230af11a8c7632189cb2b171aa59efcb24c
nareshsrkr1 <138985162+nareshsrkr1@users.noreply.github.com>
2025-01-02 00:20:44 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -3,37 +3,12 @@ DECLARE @JsonString NVARCHAR(MAX);
 -- Construct JSON using alias names for hash ID calculation
 SELECT @JsonString = STRING_AGG(
     '"' + 
-    CASE 
-        WHEN CHARINDEX(' AS ', ColumnExpression) > 0 
-        THEN RIGHT(ColumnExpression, LEN(ColumnExpression) - CHARINDEX(' AS ', ColumnExpression) - 3)
-        ELSE ColumnExpression
-    END 
-    + '":"'
-    +
-    CASE 
-        WHEN CHARINDEX(' AS ', ColumnExpression) > 0 
-        THEN LEFT(ColumnExpression, CHARINDEX(' AS ', ColumnExpression) - 1)
-        ELSE ColumnExpression
-    END 
-    + '"', ',')
-FROM (
-    SELECT 
-        REPLACE(
-            CASE 
-                WHEN CHARINDEX(' AS ', ColumnExpression) > 0 
-                THEN RIGHT(ColumnExpression, LEN(ColumnExpression) - CHARINDEX(' AS ', ColumnExpression) - 3)
-                ELSE ColumnExpression
-            END, 
-            'Exit_Period', 
-            'business_input'
-        ) AS AliasName,
-        CASE 
-            WHEN CHARINDEX(' AS ', ColumnExpression) > 0 
-            THEN LEFT(ColumnExpression, CHARINDEX(' AS ', ColumnExpression) - 1)
-            ELSE ColumnExpression
-        END AS ColumnName
-    FROM @Finallist
-) AS JsonColumns;
+    REPLACE(AliasName, 'Exit_Period', 'business_input') + '":"'
+    + ColumnName + '"', ',')
+FROM FinalColumns;
 
+-- Wrap the result in curly braces to form a valid JSON object
 SET @JsonString = '{' + @JsonString + '}';
-PRINT @JsonString; -- Debug output
+
+-- Debug output
+PRINT @JsonString;
