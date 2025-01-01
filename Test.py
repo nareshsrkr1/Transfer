4b354016cea4c71e4e7a5f53d0f8a2859d8ee345
nareshsrkr1 <138985162+nareshsrkr1@users.noreply.github.com>
2025-01-01 23:16:51 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,10 +1,19 @@
 INSERT INTO @FinalColumns (ColumnExpression, SortOrder)
 SELECT 
+    -- Add aliases if mapped, otherwise use the column name directly
     CASE 
         WHEN m.OverrideColumn IS NOT NULL THEN u.ColumnName + ' AS [' + m.OverrideColumn + ']'
         ELSE u.ColumnName
     END AS ColumnExpression,
-    CHARINDEX(',' + LOWER(ISNULL(m.OverrideColumn, u.ColumnName)) + ',', ',' + LOWER(@OrderedColumnList) + ',') AS SortOrder
+    -- Case-insensitive search for the column in @OrderedColumnList
+    CASE 
+        WHEN CHARINDEX(',' + LOWER(ISNULL(m.OverrideColumn, u.ColumnName)) + ',', ',' + LOWER(@OrderedColumnList) + ',') > 0 THEN 
+            CHARINDEX(',' + LOWER(ISNULL(m.OverrideColumn, u.ColumnName)) + ',', ',' + LOWER(@OrderedColumnList) + ',')
+        ELSE 9999 -- Assign a large number for unmatched columns
+    END AS SortOrder
 FROM @ActiveColumns u
 LEFT JOIN @MappedColumns m
-    ON LOWER(u.ColumnName) = LOWER(m.UnfdColumn);
+    ON LOWER(u.ColumnName) = LOWER(m.UnfdColumn); -- Normalize for case-insensitive join
+
+-- Debug: Output results to verify
+SELECT * FROM @FinalColumns;
