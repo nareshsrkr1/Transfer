2e1d7a320ca8449a22681b24d2d23c850e7ddc86
nareshsrkr1 <138985162+nareshsrkr1@users.noreply.github.com>
2025-03-06 11:43:10 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,24 +1,25 @@
 <div class="container-fluid">
   <div class="row d-flex justify-content-center" *ngFor="let row of rows; let i = index">
     
-    <!-- Condition Label -->
-    <label class="condition-label" [class.invisible]="i > 0">
-      Select Business Segment :
-    </label>
-    <span *ngIf="i > 0" class="and-text">AND</span>
+    <!-- Flex Container for Proper Column Alignment -->
+    <div class="d-flex align-items-center w-100">
+      
+      <!-- Label only for the first row -->
+      <label *ngIf="i == 0" class="mr-2">Select Business Segment :</label>
+      
+      <!-- AND (Visible from second row onwards) -->
+      <span *ngIf="i > 0" class="mr-2">AND</span>
 
-    <!-- Flex Container to Align Elements -->
-    <div class="d-flex align-items-center">
       <!-- Dropdown -->
       <app-select-dropdown 
         [listOfValues]="businessSegments"
         [(ngModel)]="row.selectedBusinessSegment"
-        class="ml-2">
+        class="ml-2 flex-grow-1">
       </app-select-dropdown>
 
       <!-- Search Box -->
       <input type="text" 
-        class="form-control ml-2" 
+        class="form-control ml-2 flex-grow-1" 
         style="width: 250px; height: calc(2.8rem + 1px);" 
         placeholder="Type to search" 
         [(ngModel)]="row.newBusinessSegment" />
@@ -38,6 +39,7 @@
         (click)="removeBusinessSegment(i)">
         Remove
       </button>
+
     </div>
 
   </div>
