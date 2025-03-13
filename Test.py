f9e6db9181d4716cc6dc9b4d9026bbffeac0ea2c
nareshsrkr1 <138985162+nareshsrkr1@users.noreply.github.com>
2025-03-13 15:44:57 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,89 +1,31 @@
-<div class="container-fluid">
-  <div class="row justify-content-center">
-    
-    <!-- Left Panel: Business Segments -->
-    <div class="col-md-5">
-      <h5>Business Segments</h5>
-      <div *ngFor="let row of businessRows; let i = index" class="row align-items-center mb-2">
-        
-        <app-select-dropdown [listOfValues]="businessSegments"
-          [(ngModel)]="row.selectedBusinessSegment" class="col-md-4"></app-select-dropdown>
-
-        <input type="text" class="form-control col-md-4" 
-          [ngClass]="{'short-input': i === businessRows.length - 1 && i > 0}" 
-          placeholder="Type to search" [(ngModel)]="row.newBusinessSegment"/>
-
-        <button *ngIf="i === businessRows.length - 1" class="btn btn-primary ml-2 btn-sm" 
-          (click)="addBusinessSegment()">Add</button>
-
-        <button *ngIf="i > 0" class="btn btn-danger ml-2 btn-sm" 
-          (click)="removeBusinessSegment(i)">Remove</button>
-
+<div class="container">
+  <div class="row">
+    <!-- Business Segments Section -->
+    <div class="col-md-6">
+      <h4>Business Segments</h4>
+      <div *ngFor="let row of businessRows; let i = index" class="segment-row">
+        <app-select-dropdown [listOfValues]="businessSegments" [(ngModel)]="row.selectedSegment"></app-select-dropdown>
+        <input type="text" class="search-box" [(ngModel)]="row.newBusinessSegment" placeholder="Type to search" />
+        <button *ngIf="i === businessRows.length - 1" class="btn btn-primary btn-sm" (click)="addBusinessSegment()">Add</button>
+        <button *ngIf="i > 0" class="btn btn-danger btn-sm" (click)="removeBusinessSegment(i)">Remove</button>
       </div>
     </div>
 
-    <!-- Right Panel: Overrides -->
-    <div class="col-md-5">
-      <h5>Overrides</h5>
-      <div *ngFor="let row of overrideRows; let i = index" class="row align-items-center mb-2">
-        
-        <app-select-dropdown [listOfValues]="overrideSegments"
-          [(ngModel)]="row.selectedOverrideSegment" class="col-md-4"></app-select-dropdown>
-
-        <input type="text" class="form-control col-md-4" 
-          [ngClass]="{'short-input': i === overrideRows.length - 1 && i > 0}" 
-          placeholder="Type to search" [(ngModel)]="row.newOverrideSegment"/>
-
-        <button *ngIf="i === overrideRows.length - 1" class="btn btn-primary ml-2 btn-sm" 
-          (click)="addOverrideSegment()">Add</button>
-
-        <button *ngIf="i > 0" class="btn btn-danger ml-2 btn-sm" 
-          (click)="removeOverrideSegment(i)">Remove</button>
-
+    <!-- Overrides Section -->
+    <div class="col-md-6">
+      <h4>Overrides</h4>
+      <div *ngFor="let row of overrideRows; let i = index" class="override-row">
+        <app-select-dropdown [listOfValues]="overrideSegments" [(ngModel)]="row.selectedOverride"></app-select-dropdown>
+        <input type="text" class="search-box" [(ngModel)]="row.newOverrideSegment" placeholder="Type to search" />
+        <button *ngIf="i === overrideRows.length - 1" class="btn btn-primary btn-sm" (click)="addOverrideSegment()">Add</button>
+        <button *ngIf="i > 0" class="btn btn-danger btn-sm" (click)="removeOverrideSegment(i)">Remove</button>
       </div>
     </div>
-
   </div>
 
-  <!-- AND, Submit, and Reset buttons at the bottom center -->
-  <div class="row justify-content-center mt-4">
-    <button class="btn btn-secondary mx-2">AND</button>
-    <button class="btn btn-success mx-2">Submit</button>
-    <button class="btn btn-warning mx-2">Reset</button>
+  <!-- Submit & Reset Buttons -->
+  <div class="row justify-content-center mt-3">
+    <button class="btn btn-success">Submit</button>
+    <button class="btn btn-warning ml-2">Reset</button>
   </div>
 </div>
-
-
-.short-input {
-  width: calc(250px - 50px); /* Reduce width for last-row inputs with Add + Remove */
-}
-
-.container-fluid {
-  padding: 20px;
-}
-
-h5 {
-  text-align: center;
-  margin-bottom: 10px;
-}
-
-
-
-businessRows = [{ selectedBusinessSegment: '', newBusinessSegment: '' }];
-overrideRows = [{ selectedOverrideSegment: '', newOverrideSegment: '' }];
-
-addBusinessSegment() {
-  this.businessRows.push({ selectedBusinessSegment: '', newBusinessSegment: '' });
-}
-
-removeBusinessSegment(index: number) {
-  this.businessRows.splice(index, 1);
-}
-
-addOverrideSegment() {
-  this.overrideRows.push({ selectedOverrideSegment: '', newOverrideSegment: '' });
-}
-
-removeOverrideSegment(index: number) {
-  this.overrideRows.splice(index, 1);
-}
